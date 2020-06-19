6588df1679aae23d6412c8019b50c701d55085cf
ssilb4 <ssilb4@gmail.com>
2020-06-19 10:46:51 +0900

    Update DialogflowSamples.cs

--- dialogflow/api/DialogflowSamples/DialogflowSamples.cs
+++ dialogflow/api/DialogflowSamples/DialogflowSamples.cs
@@ -1,4 +1,4 @@
-﻿// Copyright(c) 2018 Google Inc.
+// Copyright(c) 2018 Google Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not
 // use this file except in compliance with the License. You may obtain a copy of
@@ -23,8 +23,11 @@ namespace GoogleCloudSamples
         {
             var verbMap = new VerbMap<object>();
 
+
             DetectIntentTexts.RegisterCommands(verbMap);
             DetectIntentStream.RegisterCommands(verbMap);
+            DetectIntentTexttospeechResponse.RegisterCommands(verbMap);
+            DetectIntentAudioFile.RegisterCommands(verbMap);
             IntentManagement.RegisterCommands(verbMap);
 
             verbMap.NotParsedFunc = (err) => 1;
